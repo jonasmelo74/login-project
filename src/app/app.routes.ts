03a0cecec15f776f3c6e645041c0b05e2bc284dc
JonasMelo <jonasmelodapaz74@gmail.com>
2024-08-07 00:31:04 -0300

    #comment finalizando login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+<<<<<<< Updated upstream
 
 export const routes: Routes = [];
+=======
+import { LoginComponent } from './pages/login/login.component';
+import { SignUpComponent } from './pages/signup/signup.component';
+
+export const routes: Routes = [
+    {
+        path: "login",
+        component: LoginComponent
+    },
+    {
+        path: "signup",
+        component: SignUpComponent
+    }
+];
+>>>>>>> Stashed changes
